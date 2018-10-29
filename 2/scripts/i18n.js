3d650b17c421c326f252082954d477314f5e2b89
Niels NTG <NTG999@gmail.com>
2018-10-29 21:35:16 +0100

    Add en-US localisations for header slideshow

--- 2/scripts/i18n.js
+++ 2/scripts/i18n.js
@@ -7,8 +7,6 @@ var textReplacements = {
     'de-DE': {
         'title': 'Generative Gestaltung',
         'intro': 'Diese Website ergänzt das Buch »Generative Gestaltung, Creative Coding im Web« (erschienen im Verlag Hermann Schmidt, 2018) und bietet direkten Zugriff auf alle Quellcodes der im Buch beschriebenen Programme.',
-        'slide-book-store-link-herman-schmidt-verlag': 'Buch kaufen<br>@ Herman Schmidt Verlag',
-        'slide-book-store-link-amazon-de': 'Buch kaufen<br>@ Amazon Deutschland',
         'p-1-color': 'P.1. Farbe',
         'p-2-shape': 'P.2. Form',
         'p-3-type': 'P.3. Typo',
@@ -18,13 +16,107 @@ var textReplacements = {
         'm-5-data-trees': 'M.5. Baumdiagramme',
         'm-6-dynamic-data-structure': 'M.6. Dynamische Datenstrukturen',
         'imprint-title': 'Impressum',
-        'download-toc' : 'Inhaltsverzeichnis'
+        'download-toc' : 'Inhaltsverzeichnis',
+        'slide-1': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/GG_Preview_180-181.png" alt="GG_Preview_180-181">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://typografie.de/produkt/generative-gestaltung-creative-coding-im-web/" target="_blank">Buch kaufen<br>@ Herman Schmidt Verlag</a>\
+            </div>'
+        ),
+        'slide-2': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/GG_Preview_40-41.png" alt="GG_Preview_40-41">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.de/Generative-Gestaltung-Programmieren-Visualisieren-Javascript/dp/3874399028/ref=as_sl_pc_tf_til?tag=genegestwebsz-21&linkCode=w00&linkId=f6cd35873d742268a4a12132fa453301&creativeASIN=3874399028" target="_blank">Buch kaufen<br>@ Amazon Deutschland</a>\
+            </div>'
+        ),
+        'slide-3': (
+            '<div class="slide__inner">\
+                <div class="slide__background" style="background-image:url(img/header/9783874399029_Generative-Gestaltung_Creative-Coding-im-Web10.jpg)"></div>\
+                <a class="slide__link" href="https://typografie.de/produkt/generative-gestaltung-creative-coding-im-web/" target="_blank">Buch kaufen<br>@ Herman Schmidt Verlag</a>\
+            </div>'
+        ),
+        'slide-4': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/GG_Preview_186-187.png" alt="GG_Preview_186-187">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.de/Generative-Gestaltung-Programmieren-Visualisieren-Javascript/dp/3874399028/ref=as_sl_pc_tf_til?tag=genegestwebsz-21&linkCode=w00&linkId=f6cd35873d742268a4a12132fa453301&creativeASIN=3874399028" target="_blank">Buch kaufen<br>@ Amazon Deutschland</a>\
+            </div>'
+        ),
+        'slide-5': (
+            '<div class="slide__inner">\
+                <div class="slide__background" style="background-image:url(img/header/9783874399029_Generative-Gestaltung_Creative-Coding-im-Web6.jpg)"></div>\
+                <a class="slide__link" href="https://typografie.de/produkt/generative-gestaltung-creative-coding-im-web/" target="_blank">Buch kaufen<br>@ Herman Schmidt Verlag</a>\
+            </div>'
+        ),
+        'slide-6': (
+            '<div class="slide__inner">\
+                <div class="slide__background" style="background-image:url(img/header/9783874399029_Generative-Gestaltung_Creative-Coding-im-Web5.jpg)"></div>\
+                <a class="slide__link" href="https://www.amazon.de/Generative-Gestaltung-Programmieren-Visualisieren-Javascript/dp/3874399028/ref=as_sl_pc_tf_til?tag=genegestwebsz-21&linkCode=w00&linkId=f6cd35873d742268a4a12132fa453301&creativeASIN=3874399028" target="_blank">Buch kaufen<br>@ Amazon Deutschland</a>\
+            </div>'
+        ),
+        'slide-7': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/GG_Preview_26-27.png" alt="GG_Preview_26-27">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://typografie.de/produkt/generative-gestaltung-creative-coding-im-web/" target="_blank">Buch kaufen<br>@ Herman Schmidt Verlag</a>\
+            </div>'
+        ),
+        'slide-8': (
+            '<div class="slide__inner">\
+                <div class="slide__background" style="background-image:url(img/header/9783874399029_Generative-Gestaltung_Creative-Coding-im-Web3.jpg)"></div>\
+                <a class="slide__link" href="https://www.amazon.de/Generative-Gestaltung-Programmieren-Visualisieren-Javascript/dp/3874399028/ref=as_sl_pc_tf_til?tag=genegestwebsz-21&linkCode=w00&linkId=f6cd35873d742268a4a12132fa453301&creativeASIN=3874399028" target="_blank">Buch kaufen<br>@ Amazon Deutschland</a>\
+            </div>'
+        ),
+        'slide-9': (
+            '<div class="slide__inner">\
+                <div class="slide__background" style="background-image:url(img/header/9783874399029_Generative-Gestaltung_Creative-Coding-im-Web7.jpg)"></div>\
+                <a class="slide__link" href="https://typografie.de/produkt/generative-gestaltung-creative-coding-im-web/" target="_blank">Buch kaufen<br>@ Herman Schmidt Verlag</a>\
+            </div>'
+        ),
+        'slide-10': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/GG_Preview_148-149.png" alt="GG_Preview_148-149">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.de/Generative-Gestaltung-Programmieren-Visualisieren-Javascript/dp/3874399028/ref=as_sl_pc_tf_til?tag=genegestwebsz-21&linkCode=w00&linkId=f6cd35873d742268a4a12132fa453301&creativeASIN=3874399028" target="_blank">Buch kaufen<br>@ Amazon Deutschland</a>\
+            </div>'
+        ),
+        'slide-11': (
+            '<div class="slide__inner">\
+                <div class="slide__background" style="background-image:url(img/header/9783874399029_Generative-Gestaltung_Creative-Coding-im-Web9.jpg)"></div>\
+                <a class="slide__link" href="https://typografie.de/produkt/generative-gestaltung-creative-coding-im-web/" target="_blank">Buch kaufen<br>@ Herman Schmidt Verlag</a>\
+            </div>'
+        ),
+        'slide-12': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/GG_Preview_222-223.png" alt="GG_Preview_222-223">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.de/Generative-Gestaltung-Programmieren-Visualisieren-Javascript/dp/3874399028/ref=as_sl_pc_tf_til?tag=genegestwebsz-21&linkCode=w00&linkId=f6cd35873d742268a4a12132fa453301&creativeASIN=3874399028" target="_blank">Buch kaufen<br>@ Amazon Deutschland</a>\
+            </div>'
+        )
     },
     'en-US': {
         'title': 'Generative Design',
-        'intro': '<aside>👉 Generative Design: Visualize, Program, and Create with JavaScript in p5.js. Will be available in October 2018 from Princeton Architectural Press!</aside>Hello and welcome to Generative Design, Creative Coding on the Web. Here, you will find all of the sketches from the book and their associated code. Run the sketches directly in the browser with the p5.js-web-editor or locally on your machine by downloading the code package below.',
-        'slide-book-store-link-herman-schmidt-verlag': 'Order now<br>@ Herman Schmidt Verlag',
-        'slide-book-store-link-amazon-de': 'Order now<br>@ Amazon.de',
+        'intro': '<aside>👉 Generative Design: Visualize, Program, and Create with JavaScript in p5.js. Will be released on October 30 2018 by <a href="https://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Princeton Architectural Press!</a></aside>Hello and welcome to Generative Design, Creative Coding on the Web. Here, you will find all of the sketches from the book and their associated code. Run the sketches directly in the browser with the p5.js-web-editor or locally on your machine by downloading the code package below.',
         'p-1-color': 'P.1. Color',
         'p-2-shape': 'P.2. Shape',
         'p-3-type': 'P.3. Type',
@@ -34,7 +126,127 @@ var textReplacements = {
         'm-5-data-trees': 'M.5. Data trees',
         'm-6-dynamic-data-structure': 'M.6. Dynamic data structure',
         'imprint-title': 'Imprint',
-        'download-toc' : 'Table of Contents'
+        'download-toc' : 'Table of Contents',
+        'slide-1': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/180-181.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="http://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Order now<br>@ Princeton Architectural Press</a>\
+            </div>'
+        ),
+        'slide-2': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/40-41.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.com/gp/product/1616897589/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1616897589&tag=generativeges-20&linkId=52b74a301a52c167f29d53e38485d0da" target="_blank">Order now<br>@ Amazon.com</a>\
+            </div>'
+        ),
+        'slide-3': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/240-241.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="http://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Order now<br>@ Princeton Architectural Press</a>\
+            </div>'
+        ),
+        'slide-4': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/186-187.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.com/gp/product/1616897589/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1616897589&tag=generativeges-20&linkId=52b74a301a52c167f29d53e38485d0da" target="_blank">Order now<br>@ Amazon.com</a>\
+            </div>'
+        ),
+        'slide-5': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/178-179.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="http://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Order now<br>@ Princeton Architectural Press</a>\
+            </div>'
+        ),
+        'slide-6': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/144-145.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.com/gp/product/1616897589/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1616897589&tag=generativeges-20&linkId=52b74a301a52c167f29d53e38485d0da" target="_blank">Order now<br>@ Amazon.com</a>\
+            </div>'
+        ),
+        'slide-7': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/26-27.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="http://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Order now<br>@ Princeton Architectural Press</a>\
+            </div>'
+        ),
+        'slide-8': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/86-87.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.com/gp/product/1616897589/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1616897589&tag=generativeges-20&linkId=52b74a301a52c167f29d53e38485d0da" target="_blank">Order now<br>@ Amazon.com</a>\
+            </div>'
+        ),
+        'slide-9': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/184-185.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="http://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Order now<br>@ Princeton Architectural Press</a>\
+            </div>'
+        ),
+        'slide-10': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/148-149.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.com/gp/product/1616897589/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1616897589&tag=generativeges-20&linkId=52b74a301a52c167f29d53e38485d0da" target="_blank">Order now<br>@ Amazon.com</a>\
+            </div>'
+        ),
+        'slide-11': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/206-207.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="http://www.papress.com/html/product.details.dna?isbn=9781616897581" target="_blank">Order now<br>@ Princeton Architectural Press</a>\
+            </div>'
+        ),
+        'slide-12': (
+            '<div class="slide__inner">\
+                <div class="slide__background">\
+                    <div class="slide__book-img">\
+                        <img src="img/header/en/222-223.jpg">\
+                    </div>\
+                </div>\
+                <a class="slide__link" href="https://www.amazon.com/gp/product/1616897589/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1616897589&tag=generativeges-20&linkId=52b74a301a52c167f29d53e38485d0da" target="_blank">Order now<br>@ Amazon.com</a>\
+            </div>'
+        )
     }
 };
 
